a8f31d528bb16e8b809fd8c4968c315428fe5466
vansi99 <vansido99@gmail.com>
2018-09-18 22:28:41 +0700

    change

---
+++ Group2/17020998/lab02/Group.java
@@ -0,0 +1,11 @@
+public class Group {
+    private String group;
+
+    public void setGroup(String group){
+        this.group = group;
+    }
+
+    public String getGroup(){
+        return this.group;
+    }
+}
