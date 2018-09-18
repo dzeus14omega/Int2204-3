214ae48cc7213a8e99ac5f853a35bad070edd906
Thuong1998 <37398323+Thuong1998@users.noreply.github.com>
2018-09-19 00:00:45 +0700

    Add files via upload

---
+++ Group2/17020168/lab02/StudentManagement.java
@@ -0,0 +1,23 @@
+package bai_tap_tuan_2;
+
+import java.util.Scanner;
+
+public class StudentManagement {
+
+	public boolean sameGroup(Student s1,Student s2) {
+		if(s1.getGroup()==s2.getGroup())
+			return true;
+		else return false;
+		
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Student student[] = new Student[100];
+		for(int i=0;i<2;i++)
+		{
+			
+		}
+	}
+
+}
